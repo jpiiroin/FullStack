e3bf92988fdcb3c96d174aff5faa438a9eab5794
Janne Piiroinen <jpiiroin@hotmail.com>
2023-11-26 00:25:06 -0800

    1.6: unicafe step1

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      
+      <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
+
+      <h1>Statistics</h1>
+      good {good} <br/>
+      neutral {neutral} <br/>
+      bad {bad}
+    
+    </div>
+  )
+}
+
+export default App
