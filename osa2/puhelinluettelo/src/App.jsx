ca7a99372cf706682c5d0b4963097edb8c662135
Janne Piiroinen <jpiiroin@hotmail.com>
2024-03-16 09:06:44 -0700

    2.9*: puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244'}
+    { name: 'Arto Hellas', number: '040-1231244'},
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
   
   const addPerson = (event) => {
     event.preventDefault()
@@ -33,9 +37,22 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+    setNewSearch(event.target.value)
+  }
+
+  function filterItems(arr, query) {
+    return arr.filter((s) => s.name.toLowerCase().includes(query.toLowerCase()));
+  }
+    
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input
+      value={newSearch}
+      onChange={handleSearch}
+      />
       <form onSubmit={addPerson}>
         <div>
           name: <input
@@ -54,8 +71,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(item => (<p key={item.name}>{item.name} {item.number}</p>
-      ))}
+        {filterItems(persons,newSearch).map(item => (<p key={item.name}>{item.name} {item.number}</p>
+        ))}
     </div>
   )
 }
