caff6d8cf011107b988c353bbe8fa0a9d382f683
Janne Piiroinen <jpiiroin@hotmail.com>
2024-03-24 04:18:57 -0700

    2.10: puhelinluettelo step5

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244'},
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newSearch, setNewSearch] = useState('')
+const Filter = ({ newSearch, handlenewSearch }) => {
+  return (
+    <>
+    filter shown with <input
+      value={newSearch}
+      onChange={handlenewSearch}
+      />
+    </>
+  )
+}
+
+const Persons = ({ persons, newSearch }) => {
+  function filterItems(arr, query) {
+    return arr.filter((s) => s.name.toLowerCase().includes(query.toLowerCase()));
+  }
+  const filtered = filterItems(persons, newSearch)
+  return (
+    <>
+    {filtered.map(item => <p key={item.name}>{item.name} {item.number}</p>)}
+    </> 
+  )
+}
+
+const PersonForm = ({ persons, newName, newNumber, 
+  handleNameChange, handleNumberChange, setPersons, 
+  setNewName, setNewNumber }) => {
   
   const addPerson = (event) => {
     event.preventDefault()
@@ -26,7 +42,41 @@ const App = () => {
       setNewNumber('')
     }
   }
+  
+  return (
+    <>
+    <form onSubmit={addPerson}>
+      <div>
+          name: <input
+          value={newName}
+          onChange={handleNameChange}
+          />
+      </div>
+      <div>
+          number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+          />
+      </div>
+      <div>
+        <button type="submit" disabled={newName===''}>add</button>
+      </div>
+    </form>
+    </>
+  )
+}
 
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1231244'},
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewnewSearch] = useState('')
+  
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value) 
@@ -37,42 +87,30 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const handleSearch = (event) => {
+  const handlenewSearch = (event) => {
     console.log(event.target.value)
-    setNewSearch(event.target.value)
-  }
-
-  function filterItems(arr, query) {
-    return arr.filter((s) => s.name.toLowerCase().includes(query.toLowerCase()));
+    setNewnewSearch(event.target.value)
   }
     
   return (
     <div>
       <h2>Phonebook</h2>
-      filter shown with <input
-      value={newSearch}
-      onChange={handleSearch}
-      />
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input
-          value={newName}
-          onChange={handleNameChange}
-           />
-        </div>
-        <div>
-          number: <input
-          value={newNumber}
-          onChange={handleNumberChange}
-           />
-        </div>
-        <div>
-          <button type="submit" disabled={newName ===''}>add</button>
-        </div>
-      </form>
+      <Filter newSearch = {newSearch} handlenewSearch = {handlenewSearch}/>
+      
+      <h2>add a new</h2>
+      <PersonForm
+        persons = {persons}
+        newName = {newName}
+        newNumber = {newNumber}
+        handleNameChange = {handleNameChange}
+        handleNumberChange = {handleNumberChange}
+        setPersons = {setPersons}
+        setNewName = {setNewName}
+        setNewNumber = {setNewNumber}
+        />
+
       <h2>Numbers</h2>
-        {filterItems(persons,newSearch).map(item => (<p key={item.name}>{item.name} {item.number}</p>
-        ))}
+      <Persons persons = {persons} newSearch = {newSearch}/>
     </div>
   )
 }
